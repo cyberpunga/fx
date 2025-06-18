1bb0481f3b1d9bca01bcb5e22ae567cb739f2dec
Rodrigo Moreira <hola@piducan.dev>
2025-06-18 12:54:59 -0400

    update base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://cyberpun.ga/fx",
+  base: "https://cyberpunga.github.io/fx",
 });
