f119cc2535fa3f494e98d85f545531194062775a
Rodrigo Moreira <hola@piducan.dev>
2026-05-16 23:56:06 -0400

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://cyberpunga.github.io/fx",
+  base: "https://cyberpun.ga/fx",
 });
